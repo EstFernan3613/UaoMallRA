01c966735b84a1d055eb216fc13f22dd9e892687
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-09-26 19:44:25 -0500

    Cambios

--- Assets/Scripts/ButtonSpeech.cs
+++ Assets/Scripts/ButtonSpeech.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ButtonSpeech : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public UnityEvent OnButtonPressed;
     public UnityEvent OnButtonReleased;
 
+    public Text txt;
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        txt.text = "Escuchanding...";
         OnButtonPressed?.Invoke();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        txt.text = " ";
         OnButtonReleased?.Invoke();
     }
 
